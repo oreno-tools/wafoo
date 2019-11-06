d13e9d1c91ba7f8d3bc3067934eff192f44ef421
inokappa <inokara@gmail.com>
2019-11-06 20:56:51 +0900

    add dummy aws credentials

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,9 @@ require 'rspec'
 require 'wafoo'
 
 Aws.config.update(stub_responses: true)
-ENV['AWS_PROFILE'] = 'dummy_profile'
+# ENV['AWS_PROFILE'] = 'dummy_profile'
+ENV['AWS_ACCESS_KEY_ID'] = 'XXXXXXXXXXXXXXXXXXXX'
+ENV['AWS_SECRET_ACCESS_KEY'] = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
 ENV['AWS_REGION'] = 'us-east-1'
 ENV['LOAD_STUB'] = 'true'
 
